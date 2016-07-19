a35b0816770b7c56b8efaee8449baaec2d688a71
Ricardo Pandales <rpandales@pros.com>
2016-07-19 14:53:03 -0500

    user-form: Make the register form a tab

--- app/scripts/services/account.js
+++ app/scripts/services/account.js
@@ -9,11 +9,15 @@
  */
 angular.module('meetUpEventPlannerApp')
   .service('account', function () {
+  	var id = 1;
     return {
       create: function (account) {
         console.log(account);
       },
       login: function (userInfo) {
+
+      },
+      get: function (id) {
 
       }
 
